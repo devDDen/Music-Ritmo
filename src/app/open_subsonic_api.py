8fe0a991f28bd95f7aa49473fea313e23095ce00
Denis Borisov <defdden@mail.ru>
2025-03-10 01:16:25 +0300

    [mus-220] Implement get_album_list BY_GENRE

--- src/app/open_subsonic_api.py
+++ src/app/open_subsonic_api.py
@@ -502,7 +502,9 @@ def get_album_list(
             request_type = service_layer.RequestType.BY_ARTIST
         case "byYear":
             request_type = service_layer.RequestType.BY_YEAR
-        case "newest" | "highest" | "frequent" | "recent" | "byGenre":
+        case "byGenre":
+            request_type = service_layer.RequestType.BY_GENRE
+        case "newest" | "highest" | "frequent" | "recent":
             # Not implemented
             request_type = service_layer.RequestType.BY_NAME
         case _:
@@ -542,7 +544,9 @@ def get_album_list2(
             request_type = service_layer.RequestType.BY_ARTIST
         case "byYear":
             request_type = service_layer.RequestType.BY_YEAR
-        case "newest" | "highest" | "frequent" | "recent" | "byGenre":
+        case "byGenre":
+            request_type = service_layer.RequestType.BY_GENRE
+        case "newest" | "highest" | "frequent" | "recent":
             # Not implemented
             request_type = service_layer.RequestType.BY_NAME
         case _:
--- tests/unit/test_AlbumService.py
+++ tests/unit/test_AlbumService.py
@@ -228,7 +228,7 @@ class TestTrackService(unittest.TestCase):
             (None, None),
         ]
     )
-    def test_get_album_list_failed(
+    def test_get_album_list_by_year_failed(
         self,
         from_year: str | None,
         to_year: str | None,
@@ -246,6 +246,34 @@ class TestTrackService(unittest.TestCase):
 
         self.assertIsNone(result)
 
+    def test_get_album_list_by_genre(self):
+        album, _, _ = get_entities(1)
+
+        self.album_service.album_db_helper.get_albums_by_genre = MagicMock(
+            return_value=[album]
+        )
+
+        result = self.album_service.get_album_list(
+            RequestType.BY_GENRE, size=10, offset=0, genre="mygenre"
+        )
+
+        self.assertIsNotNone(result)
+        self.assertEqual(len(result), 1)
+        self.check_album(result[0], album, with_tracks=False)
+
+    def test_get_album_list_by_genre_failed(self):
+        album, _, _ = get_entities(1)
+
+        self.album_service.album_db_helper.get_albums_by_genre = MagicMock(
+            return_value=[album]
+        )
+
+        result = self.album_service.get_album_list(
+            RequestType.BY_GENRE, size=10, offset=0, genre=None
+        )
+
+        self.assertIsNone(result)
+
     def test_get_sorted_artist_albums(self):
         album, _, _ = get_entities(1)
 
@@ -258,6 +286,7 @@ class TestTrackService(unittest.TestCase):
         self.assertIsNotNone(result)
         self.assertEqual(len(result), 1)
         self.check_album(result[0], album, with_tracks=False)
+
 
 if __name__ == "__main__":
     unittest.main()
